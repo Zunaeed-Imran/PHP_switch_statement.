d1773e5cce30fa028c933bb51a29cbee1949154a
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-06 12:38:39 +0600

    date in switch statement.

---
+++ date.php
@@ -0,0 +1,14 @@
+<?php
+$date = date("l"); 
+$date = "Thursday";
+
+switch($date){
+ case "Monday":
+  echo "I like to work today";
+  break;
+ case "Thursday":
+  echo "wow is't weekend";
+  break;
+}
+
+?>
