cd9a54134e74ce0e241b64bdbcec1bf198bb167d
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-06 12:15:51 +0600

    switch statement using grade point

---
+++ switch.php
@@ -0,0 +1,23 @@
+<?php
+  $grade = "f";
+  switch($grade){
+    case "A":
+      echo "you did grate";
+      break;
+    case "B":
+      echo "You did good";
+      break;
+    case "C":
+      echo "You did okey";
+      break;
+    case "D":
+      echo "You did poorly";
+      break;
+    case "F":
+      echo "You have faield";
+      break;   
+    default:
+      echo "Not valid input";       
+  }
+
+?>
